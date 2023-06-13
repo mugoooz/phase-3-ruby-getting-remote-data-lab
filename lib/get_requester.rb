f76dffb71089ab95977c86820591861f1fc175f4
Eric Mugo <mugoericpg20@gmail.com>
2023-06-13 11:56:13 +0300

    done

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -2,18 +2,18 @@ require 'net/http'
 require 'json'
 
 class GetRequester
-    def initialize(url)
-        @url = url
-    end
+  def initialize(url)
+    @url = url
+  end
 
-    def get_response_body
-        uri = URI.parse(@url)
-        response = Net:HTTP.get_response(uri)
-        response.body
-    end
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
 
-    def parse_json
-        response_body = get_response_body
-        JSON.parse(response_body)
-    end
+  def parse_json
+    response_body = get_response_body
+    JSON.parse(response_body)
+  end
 end
